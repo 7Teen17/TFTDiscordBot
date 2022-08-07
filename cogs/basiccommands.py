eba2e608c69dbb62ac1670c549055fbc298a3172
7Teen17 <7Teen17.email@gmail.com>
2022-08-07 14:21:04 -0700

    changed some things

--- cogs/basiccommands.py
+++ cogs/basiccommands.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
-from main import admin_command
+from main import admin_command, botcommands_command
 
-class Purge(commands.Cog):
+class BasicCommands(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
@@ -11,6 +11,11 @@ class Purge(commands.Cog):
     await ctx.message.delete()
     await ctx.channel.purge(limit=number)
     await ctx.send(f"Deleted {number} messages.", delete_after=3)
+
+  @commands.command()
+  @botcommands_command()
+  async def source(self, ctx):
+    await ctx.send("https://github.com/7Teen17/TFTDiscordBot")
     
 async def setup(bot):
-  await bot.add_cog(Purge(bot))
+  await bot.add_cog(BasicCommands(bot))
